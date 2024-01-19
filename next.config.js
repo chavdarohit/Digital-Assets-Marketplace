0497303c1d7a7064c484a278a9ddb01ddf8fd6bd
Panchhiwala Devarshee <84611501+devrshee12@users.noreply.github.com>
2024-01-19 14:14:36 +0530

    config updated

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
 			},
 			{
 				protocol: "https",
-				hostname: "digitalhippo-production.up.railway.app",
+				hostname: "digitalmarketplace-production-08eb.up.railway.app",
 			},
 		],
 	},
